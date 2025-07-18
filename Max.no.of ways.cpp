08b57f3436d6c1e8e3302e9d2d7e026b32a9623e
Jannatul Ferdous Oni <jannatuloni41@gmail.com>
2025-07-18 16:44:31 +0600

    Update Max.no.of ways.cpp

--- Max.no.of ways.cpp
+++ Max.no.of ways.cpp
@@ -16,11 +16,11 @@ int main()
     int a;
     cout <<"Enter the amount: ";
     cin >> a;
-    int table[n+1][a+1];
+    int table[n][a+1];
 
-    for(int i=0; i<=n; i++)
+    for(int i=0; i<n; i++)
     {
-        table[i][0]=0;
+        table[i][0]=1;
     }
     for(int j=1; j<=a; j++)
     {
@@ -34,7 +34,7 @@ int main()
         }
     }
 
-    for(int i=1; i<=n; i++)
+    for(int i=1; i<n; i++)
     {
         for(int j=1; j<=a; j++)
         {
